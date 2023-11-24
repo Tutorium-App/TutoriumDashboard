50d4909a63e407f6e657d8380e8083f4a03d4ff1
Mubarak Tijani <116168360+Xenongt1@users.noreply.github.com>
2023-11-24 21:14:15 +0100

    hello

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    
+      'node_modules/flowbite-react/lib/esm/**/*.js'
+  
+  ],
+  theme: {
+    extend: {
+      margin: {
+        '37': '8.75rem', // You can adjust the value according to your needs
+      },
+    },
+  },
+ 
+
+    plugins: [
+        require('flowbite/plugin'),
+        require('flowbite/plugin')({
+          charts: true,
+      }),
+    ]
+
+
+}
+
