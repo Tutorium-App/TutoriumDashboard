d2b4befac8c573c174cbaf4fb2c2944b6e1a19ea
Bankah Anthony Bekoe <107210739+bankah-junior@users.noreply.github.com>
2023-12-09 17:08:44 +0000

    Added Logo, Imags and added list to dropdown menu

--- src/pages/ManageVideos/VideosDB.js
+++ src/pages/ManageVideos/VideosDB.js
@@ -1,134 +1,144 @@
+import blueAcademics from '../../assets/images/blue_academics.jpeg';
+import pinkAcademics from '../../assets/images/pink_academics.jpeg';
+import greenAcademics from '../../assets/images/green_academics.jpeg';
+import blueOthers from '../../assets/images/blue_others.jpeg';
+import blueSkills from '../../assets/images/blue_skills.jpeg';
+import greenOthers from '../../assets/images/green_others.jpeg';
+import greenSkills from '../../assets/images/green_skills.jpeg';
+import pinkOthers from '../../assets/images/pink_others.jpeg';
+import pinkSkills from '../../assets/images/pink_skills.jpeg';
+
 const VideosDB = [
-    {
-      id: 0,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Mubarak Green",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 1,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Lawson Green",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 2,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Foster Green",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 3,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Ben Green",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 4,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Benny Green",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 5,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Sam Green",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 6,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Mubarak Blue",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 7,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Lawson Blue",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 8,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Red Foster",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 9,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Yellow Sammy",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 10,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Yellow Ben",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 11,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Benny Blue",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 12,
-      title: "Arduino",
-      tag: "Videos",
-      author: "Lonnie Green",
-      category: "Skills",
-      rating: 4,
-      imageUrl:
-        "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
+  {
+    id: 0,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Mubarak Green",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      blueOthers,
+  },
+  {
+    id: 1,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Lawson Green",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      pinkSkills,
+  },
+  {
+    id: 2,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Foster Green",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      greenAcademics,
+  },
+  {
+    id: 3,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Ben Green",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      pinkAcademics,
+  },
+  {
+    id: 4,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Benny Green",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      greenAcademics,
+  },
+  {
+    id: 5,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Sam Green",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      blueOthers,
+  },
+  {
+    id: 6,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Mubarak Blue",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      greenOthers,
+  },
+  {
+    id: 7,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Lawson Blue",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      blueSkills,
+  },
+  {
+    id: 8,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Red Foster",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      pinkSkills,
+  },
+  {
+    id: 9,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Yellow Sammy",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      blueSkills,
+  },
+  {
+    id: 10,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Yellow Ben",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      greenSkills,
+  },
+  {
+    id: 11,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Benny Blue",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      pinkOthers,
+  },
+  {
+    id: 12,
+    title: "Arduino",
+    tag: "Videos",
+    author: "Lonnie Green",
+    category: "Skills",
+    rating: 4,
+    imageUrl:
+      blueAcademics,
+  },
   ];
 
 export default VideosDB
